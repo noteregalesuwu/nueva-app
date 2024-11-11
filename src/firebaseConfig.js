51f29be4bb2867341b329cf802c96fa03d7dc7e8
Nutria <holy.feik@gmail.com>
2024-11-11 00:11:22 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDl54mZdeyMRL8gf4WKUxkC96b8HaY28hg",
-  authDomain: "nutrias-chambeadoras.firebaseapp.com",
+  apiKey: "xxxxxxxxx",
+  authDomain: "xxx",
   projectId: "nutrias-chambeadoras",
-  storageBucket: "nutrias-chambeadoras.firebasestorage.app",
-  messagingSenderId: "1003209735962",
-  appId: "1:1003209735962:web:55cc121df1cff1ed1076a3",
-  measurementId: "G-MZNGQJY4DK"
+  storageBucket: "xxxxxx",
+  messagingSenderId: "xxxxx",
+  appId: "xxxx",
+  measurementId: "xxxxx"
 };
 
 // Initialize Firebase
